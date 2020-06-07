9c82bc808af3dc71ff8494808a7a7b4b2e3d981e
AmadoJunior <amadoelrepalol@gmail.com>
2020-06-07 15:07:45 -0400

    Added base case for numbers that are less than zero

--- Recursion/OptionalRecusionProblems/factorial.js
+++ Recursion/OptionalRecusionProblems/factorial.js
@@ -1,11 +1,12 @@
 function factorial(num){  
-
-    if(num === 1){
+    if(num < 0){
+        return 0;
+    }
+    if(num <= 1){
         return 1;
     }
     
     return num * factorial(num-1);
-
 }
 
 console.log(factorial(5));
