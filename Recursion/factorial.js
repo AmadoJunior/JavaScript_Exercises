f2847593869e55514d85fd9ed1083cb182bdfcaa
AmadoJunior <amadoelrepalol@gmail.com>
2020-06-07 14:06:32 -0400

    Factorial recursion

--- Recursion/factorial.js
+++ Recursion/factorial.js
@@ -1,8 +1,8 @@
 function factorial(num){
-    if(num === 2){
-        return 2;
+    if(num === 1){
+        return 1;
     }
     return num * factorial(num-1);
 }
 
-console.log(factorial(4))
+console.log(factorial(1))
