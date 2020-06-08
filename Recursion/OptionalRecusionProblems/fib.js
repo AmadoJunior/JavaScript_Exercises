03fda8e4015d90f66361ca46be6d5c2609b47502
Amado.Dominguez001 <amado.dominguez001@mymdc.net>
2020-06-07 20:05:46 -0400

    Fibonacci Sequence example added

---
+++ Recursion/OptionalRecusionProblems/fib.js
@@ -0,0 +1,10 @@
+/**
+ * Write a recursive function called fib which accepts a number and returns:
+ * the nth number in the Fibonacci sequence.
+ */
+function fib(n){
+    if(n <= 2) return 1;
+    return fib(n-1) + fib(n-2);
+}
+
+console.log(fib(4));
