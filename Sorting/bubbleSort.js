7024754149e34caa3eb669e90b03ce0e04ba9c1e
AmadoJunior <amadoelrepalol@gmail.com>
2020-06-12 18:08:04 -0400

    Added swap method to blubbleSort for reability.

--- Sorting/bubbleSort.js
+++ Sorting/bubbleSort.js
@@ -1,10 +1,20 @@
+/**
+ * In a bubble sort the largest value bubbles to the end of the array, we then ignore that value
+ * and run the same process on the rest of the array.
+ */
+
+function swap(arr, index1, index2){
+    let temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+
+}
+
 function bubbleSort(arr){
     for(let i = 0; i < arr.length-1; i++){
         for(let j = 0; j < arr.length-i-1; j++){
             if(arr[j] > arr[j+1]){
-                let temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                swap(arr, j, j+1);
             }
         }
     }
