cafd61a8833da7f9ba58780f1cb63a74d9ffd782
AmadoJunior <amadoelrepalol@gmail.com>
2020-06-12 16:41:07 -0400

    Added binary search

---
+++ Sorting/BinarySearch.js
@@ -0,0 +1,18 @@
+function BinarySort(arr, num){
+    let left = 0;
+    let right = arr.length-1;
+    let middle;
+    while(left <= right){
+        middle = Math.floor(right - left / 2);
+        if(arr[middle]=== num){
+            return middle;
+        } else if (arr[middle] < num){
+            left = middle + 1;
+        } else {
+            right = middle - 1;
+        }
+    }
+    return null;
+}
+
+console.log(BinarySort(["a","b","c","x","y","z"], "y"));
---
+++ Sorting/StringCompare.js
@@ -0,0 +1,4 @@
+let str1  = "a";
+let str2 = "b";
+
+console.log(str1 < str2 ? true: false)
