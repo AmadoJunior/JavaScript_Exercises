88caa4848173db6f005862d815e8d11a79589894
AmadoJunior <amadoelrepalol@gmail.com>
2020-06-12 18:16:05 -0400

    noSwaps optimization added to bubble sort

--- Sorting/bubbleSort.js
+++ Sorting/bubbleSort.js
@@ -1,6 +1,9 @@
 /**
  * In a bubble sort the largest value bubbles to the end of the array, we then ignore that value
  * and run the same process on the rest of the array.
+ * 
+ * If no swaps are done then we can break because that means that the array if already sorted
+ * even though we have not reached the end of it.
  */
 
 function swap(arr, index1, index2){
@@ -9,13 +12,19 @@ function swap(arr, index1, index2){
     arr[index2] = temp;
 
 }
-
+//Optimized with noSwaps
 function bubbleSort(arr){
+    let noSwaps;
     for(let i = 0; i < arr.length-1; i++){
+        noSwaps = true;
         for(let j = 0; j < arr.length-i-1; j++){
             if(arr[j] > arr[j+1]){
                 swap(arr, j, j+1);
+                noSwap = false;
             }
+        }
+        if(noSwaps){
+            break;
         }
     }
     return arr;
