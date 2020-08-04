7b64def819c2882ef6d0c4502eb69e1056233333
AmadoJunior <amadoelrepalol@gmail.com>
2020-08-04 07:29:17 -0400

    Tree Traversal section finished.

--- DataStructs/Trees/BinarySearchTree.js
+++ DataStructs/Trees/BinarySearchTree.js
@@ -56,11 +56,82 @@ class BST{
         }
         return undefined;
     }
+    BFS(){
+        let queue = [];
+        let visited = [];
+        let curNode = this.root;
+
+        queue.push(curNode);
+        while(queue.length){
+            curNode = queue.shift();
+            visited.push(curNode);
+            if(curNode.left){
+                queue.push(curNode.left);
+            }
+            if(curNode.right){
+                queue.push(curNode.right);
+            }
+        }
+        return visited;
+    }
+    DFSPreOrder(){
+        let visited = [];
+        let curNode = this.root;
+        function helper(node){
+            visited.push(node.val);
+            
+            if(node.left){
+                helper(node.left);
+            }
+
+            if(node.right){
+                helper(node.right);
+            }
+        }
+        helper(curNode);
+        return visited;
+    }
+    DFSPostOrder(){
+        let visited = [];
+        let curNode = this.root;
+        function helper(node){
+            if(node.left){
+                helper(node.left);
+            }
+
+            if(node.right){
+                helper(node.right);
+            }
+
+            visited.push(node.val);
+        }
+        helper(curNode);
+        return visited;
+    }
+    DFSInOder(){
+        let visited = [];
+        let curNode = this.root;
+        function helper(node){
+            if(node.left){
+                helper(node.left);
+            }
+
+            visited.push(node.val);
+
+            if(node.right){
+                helper(node.right);
+            }
+        }
+        helper(curNode);
+        return visited;
+    }
 }
 
 let myTree = new BST();
-myTree.insert(5)
+myTree.insert(10)
+myTree.insert(6)
+myTree.insert(15)
 myTree.insert(3)
 myTree.insert(8)
-myTree.insert(1)
-console.log(myTree.find(0));
+myTree.insert(20)
+console.log(myTree.DFSInOder());
